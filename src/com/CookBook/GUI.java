3eab922af04bdcf73fcf0f61767c44bbc352a882
Jarosław Gawęda <jaroslaw.gaw@gmail.com>
2022-03-08 09:30:48 +0100

    simple gui with button

---
+++ src/com/CookBook/GUI.java
@@ -0,0 +1,48 @@
+package com.CookBook;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GUI implements ActionListener {
+
+    private int count = 0;
+    private JLabel label;
+    private JFrame frame;
+    private JPanel panel;
+
+    public GUI() {
+
+        frame = new JFrame();
+
+        JButton button = new JButton("Click");
+        button.addActionListener(this);
+        label = new JLabel("Nr of button clicked: 0");
+
+
+        panel = new JPanel();
+        panel.setBorder(BorderFactory.createEmptyBorder(30, 60, 10, 60));
+        panel.setLayout(new GridLayout(0, 1));
+        panel.add(button);
+        panel.add(label);
+
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Recipe Match");
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+
+    public static void main(String[] args) {
+        new GUI();
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        count++;
+        label.setText("Nr of button clicked: " + count);
+    }
+}
